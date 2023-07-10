07f7008b807373974e42b2de11718920639eb7c5
demasiCodes <112286074+demasiCodes@users.noreply.github.com>
2023-07-10 12:34:18 -0400

    Added html, css, js files

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var titleElement = document.getElementById('title');
+    var bodyElement = document.getElementById('body');
+
+    // Update the title
+    titleElement.textContent = 'New App Title';
+
+    // Update the body
+    bodyElement.textContent = 'This is the new app body.';
+});
